8142e7d4070900b0e1c041b735c1a5754c7d659b
FarizFadliRafiudin <97866785+FarizFadliRafiudin@users.noreply.github.com>
2022-09-20 20:41:10 +0700

    Add files via upload

--- input-datadiri.php
+++ input-datadiri.php
@@ -1,21 +1,6 @@
-<form action="input-datadiri.php" method="POST">
-    <label for="nis">Nomor Induk Siswa :</label>
-    <input type="number" name="nis" placeholder="Ex.12001142"><br>
-
-    <label for="nama">Nama lengkap :</label>
-    <input type="text" name="nama" placeholder="Ex.Firdaus"><br>
-
-    <label for="tanggal_lahir">Tanggal Lahir :</label>
-    <input type="date" name="tanggal_lahir"><br>
-
-    <label for="nilai">Nilai :</label>
-    <input type="number" name="nilai" placeholder="Ex. 80.56"><br>
-
-    <input type="submit" name="simpan" value="simpan data"><br>
-</form>
-
 <?php
     include('./input-config.php');
+    echo "<a href='input-datadiri-tambah.php'>Tambah Data</a>";
     echo "<hr>";
     // Menampilkan data dari database
     $no = 1;
@@ -28,6 +13,12 @@
              <td>".$row["namalengkap"]."</td>
              <td>".$row["tanggal_lahir"]."</td>
              <td>".$row["nilai"]."</td>
+             <td>
+                <a href='input-datadiri-edit.php?nis=".$row["nis"]."'>Edit</a>
+                &nbsp; -&nbsp;
+                <a href= 'input-datadiri-hapus.php?nis=".$row["nis"]."'
+                onclick='return confirm(\"Certo Eliminare ?\");'>Hapus</a>
+             </td>
             </tr> 
         ";
         $no++;
@@ -37,9 +28,10 @@
           <table cellpadding=5 border=1 cellspacing=0>
                 <tr>
                    <th>NIS</th> 
-                   <th>Nama Lengkap</th> 
-                   <th>Tanggal Lahir</th> 
-                   <th>Nilai</th> 
+                   <th>Nome</th> 
+                   <th>Data Di Nascita</th> 
+                   <th>Punto</th> 
+                   <th>Azione</th>
                 </tr>
                 $tabledata
           </table>
